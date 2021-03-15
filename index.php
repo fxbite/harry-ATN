a9bb7cf88ec5221a498db585392efa8339a65f84
Zenotorith <65653624+Zenotorith@users.noreply.github.com>
2021-03-15 15:55:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,17 +21,6 @@
               </div>
             </div>
     
-            <div class="carousel-item carousel-image-2">
-              <div class="container">
-                <div class="carousel-caption d-none d-sm-block mb-5">
-                  <h1 class="display-3 h-color">Heading Two</h1>
-                  <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione
-                    quod aliquid inventore vero perspiciatis.</p>
-                  <a href="#" class="btn btn-color slide-btn btn-lg">Learn More</a>
-                </div>
-              </div>
-            </div>
-    
             <div class="carousel-item carousel-image-3">
               <div class="container">
                 <div class="carousel-caption d-none d-sm-block text-right mb-5">
@@ -43,14 +32,6 @@
               </div>
             </div>
           </div>
-    
-          <a href="#Carousel" data-slide="prev" class="carousel-control-prev">
-            <span class="carousel-control-prev-icon"></span>
-          </a>
-    
-          <a href="#Carousel" data-slide="next" class="carousel-control-next">
-            <span class="carousel-control-next-icon"></span>
-          </a>
         </div>
       </section>
       <!--End of slider-->
