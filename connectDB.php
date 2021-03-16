0b3ee38a705bc1a89206d1b90aede303987a8812
fxbite <letruonghthinh1703@gmail.com>
2021-03-16 20:39:36 +0700

    upload project

--- connectDB.php
+++ connectDB.php
@@ -3,9 +3,9 @@ function connectDB() {
     $con = pg_connect('dbname=d2hajjn501jrck host=ec2-54-161-239-198.compute-1.amazonaws.com port=5432 user=zsotcfxkvgqymw password=0a33c4b8f9ff68a68e55024cce26ebd73788f5bb5d6f1b25ffffa46b8bc21d0a sslmode=require');
 
     if (!$con) {
-    echo "Database connection failed.";
+        echo "Database connection failed.";
     } else {
-     echo "Database connection success.";
-}
+        return $con;
+    }
 }
 ?>
